3b8485ae66a4f7a6b3043ea7c788f152cbaacc87
claudiob <claudiob@gmail.com>
2009-09-17 17:42:01 +0200

    Added background job (Bj) that calles Radiotagmap::update_kml to create the overlay.kml file read by the app to draw the colored map

---
+++ config/initializers/bj.rb
@@ -0,0 +1,9 @@
+# This initializer runs at Rails boot time a Radiotagmap process that creates
+# and keeps updated a KML file which represents each U.S. state with a color
+# indicative of the most played genre/tag by its FM radios in that moment
+
+kml_path = "kml/overlay.kml" # TODO: DRY this, also used in views/usradio/index
+command = "Radiotagmap::update_kml '#{Rails.root.join('public', kml_path)}'" # Add forever, tags
+puts "Loading radiotagmap process (#{command})"
+ActiveRecord::Base.connection.execute("delete from bj_job;")
+Bj.submit "ruby -r rubygems -e \"require 'radiotagmap'; #{command};\""
