360447811beb60eefbccabe5436a64ffffedf7cb
claudiob <claudiob@gmail.com>
2009-09-17 21:11:51 +0200

    Fixed bj.rb not to use Bj tables before their creations

--- config/initializers/bj.rb
+++ config/initializers/bj.rb
@@ -5,5 +5,11 @@
 kml_path = "kml/overlay.kml" # TODO: DRY this, also used in views/usradio/index
 command = "Radiotagmap::update_kml '#{RAILS_ROOT}/public/#{kml_path}'" # Add forever, tags
 puts "Loading radiotagmap process (#{command})"
-ActiveRecord::Base.connection.execute("delete from bj_job;")
+
+# To prevent pending processes from queueing, clear every pending job
+begin
+  ActiveRecord::Base.connection.execute("delete from bj_job;")
+rescue ActiveRecord::StatementInvalid # In case the table does not yet exist
+end
+
 Bj.submit "ruby -r rubygems -e \"require 'radiotagmap'; #{command};\""
