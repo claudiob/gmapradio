d6f2438c5382d08d1b641dfbb304db654429127a
claudiob <claudiob@gmail.com>
2009-09-18 12:53:16 +0200

    Require radiotagmap 0.0.7 (one genre, more contrast)

--- config/initializers/bj.rb
+++ config/initializers/bj.rb
@@ -3,13 +3,14 @@
 # indicative of the most played genre/tag by its FM radios in that moment
 
 kml_path = "kml/overlay.kml" # TODO: DRY this, also used in views/usradio/index
-command = "Radiotagmap::update_kml '#{RAILS_ROOT}/public/#{kml_path}'" # Add forever, tags
+command = "Radiotagmap::update_kml '#{RAILS_ROOT}/public/#{kml_path}', [['Pop']]" # Add forever, tags
 puts "Loading radiotagmap process (#{command})"
 
 # To prevent pending processes from queueing, clear every pending job
 begin
   ActiveRecord::Base.connection.execute("delete from bj_job;")
   Bj.submit "ruby -r rubygems -e \"require 'radiotagmap'; #{command};\" > #{RAILS_ROOT}/log/radiotagmap.log 2>&1"
+  # TODO: In production the log should only report when 50 states have turned
 rescue ActiveRecord::StatementInvalid # In case the table does not yet exist
 end
 
