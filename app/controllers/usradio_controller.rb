3ebf8027dda8eee5202c6f8da38d50e04f994a99
claudiob <claudiob@gmail.com>
2009-09-30 15:20:34 -0500

    Added index.xml with overlay.kml?Time.msec to prevent Google caching

--- app/controllers/usradio_controller.rb
+++ app/controllers/usradio_controller.rb
@@ -2,6 +2,10 @@ class UsradioController < ApplicationController
   def index
     api_keys = YAML.load_file "#{RAILS_ROOT}/config/gmaps_api_key.yaml" 
     @api_key = api_keys[request.host] # or request.host_with_port
+    respond_to do |format|
+      format.html
+      format.xml
+    end    
   end
 
   def update
